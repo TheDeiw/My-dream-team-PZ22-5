167ae03a29797a8fd6b60a20288cf02740f48f82
roman fedor <romansfedor@gmail.com>
2024-11-20 10:26:33 +0200

    Add Name Surname Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/photo_5325650960917325998_x.jpg"} alt="Fedorchuk Roman" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Fedorchuk Roman</h2>
+          <p style={styles.position}>Buisness Analysyst</p>
         </div>
       </div>
     </main>
